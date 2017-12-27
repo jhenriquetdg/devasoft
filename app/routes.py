4dec21383b7856ddd1ebb732869e18d268cbe018
Zé Henrique <jhenriquetdg@gmail.cm>
2017-12-27 20:14:26 -0300

    This is half of the 3rd part of Miguel Grinberg tutorial.
    We have made advances toward form submission.
    
    Signed-off-by: Zé Henrique <jhenriquetdg@gmail.cm>

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,34 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Zetdg'}
+    user = {"username": "Zetdg"}
     posts = [
-    	{
-    		'author': {'username': 'John'},
-			'body': 'Deus é forte!'
-    	},
+        {
+            "author": {"username": "John"},
+            "body": "Deus é forte!"
+        },
 
-    	{
-    		'author': {'username': 'Susan'},
-    		'body': 'Cristo salva.'    		
-    	}
+        {
+            "author": {"username": "Susan"},
+            "body": "Cristo salva."
+        }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login request for user {} with {}, remember_me={}".format(
+            form.username.data,
+            form.password.data,
+            form.remember_me.data))
+        return redirect("/index")
+
+    return render_template("login.html", title="Sign In", form=form)
