57ca341e9e62b8bf6dcd63834e79f92be821bc5c
Zé Henrique <jhenriquetdg@gmail.cm>
2017-12-27 20:51:53 -0300

    End of 3rd tutorial exercise (Web Forms)
    Signed-off-by: Zé Henrique <jhenriquetdg@gmail.cm>

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -24,11 +24,15 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    flash("Login...")
     if form.validate_on_submit():
+        flash("succeed ")
         flash("Login request for user {} with {}, remember_me={}".format(
             form.username.data,
             form.password.data,
             form.remember_me.data))
-        return redirect("/index")
+        return redirect(url_for("index"))
+    else:
+        flash("form.")
 
     return render_template("login.html", title="Sign In", form=form)
