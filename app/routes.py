716035a508475121b8cb64383df66bae5512fdfe
Zé Henrique <jhenriquetdg@gmail.cm>
2017-12-27 15:53:58 -0300

    This is the end of the tutorial part II! I am loving it! Zé

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Hello Flask"
+    user = {'username': 'Zetdg'}
+    posts = [
+    	{
+    		'author': {'username': 'John'},
+			'body': 'Deus é forte!'
+    	},
+
+    	{
+    		'author': {'username': 'Susan'},
+    		'body': 'Cristo salva.'    		
+    	}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
